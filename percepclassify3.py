65678b2f8fbb2f1245a1e735fc15832923ec925c
Sachin Malhotra <sachinma@usc.edu>
2018-04-22 13:07:05 -0700

    Bare bones, no numpy, term-freq based

--- percepclassify3.py
+++ percepclassify3.py
@@ -1,19 +1,15 @@
-import numpy
 import ast
 import sys
 import util3
+from collections import Counter
 
 class PerceptronClassify:
-
-    UNK = "unk"
-
     def __init__(self, model_filename, test_data, output_file):
         self.model_filename = model_filename
         self.weight_vector = None
         self.bias = None
         self.test_file = test_data
         self.output_file = output_file
-        self.one_hot_vectors = {}
         self.training_unique_words = None
 
     def classify(self):
@@ -26,41 +22,22 @@ class PerceptronClassify:
                     true_or_fake, pos_or_neg = self.classify_review(review)
                     o.write(identifier + " " + true_or_fake + " " + pos_or_neg + "\n")
 
-    def create_one_hot_vectors(self):
-        for index, word in enumerate(self.training_unique_words):
-            zeros = numpy.zeros((1, len(self.training_unique_words)))
-            zeros.put(index, 1.0)
-            self.one_hot_vectors[word] = zeros
-
     def load(self):
         with open(self.model_filename) as f:
             self.training_unique_words = ast.literal_eval(f.readline())
-            self.weight_vector = [numpy.asarray(ast.literal_eval(f.readline()), dtype='float64').reshape((-1, 1)),
-                                  numpy.asarray(ast.literal_eval(f.readline()), dtype='float64').reshape((-1, 1))]
+            self.weight_vector = [ast.literal_eval(f.readline()), ast.literal_eval(f.readline())]
             self.bias = [float(f.readline()), float(f.readline())]
-            self.create_one_hot_vectors()
 
     def classify_review(self, review):
+        counts = Counter(review)
+        activation1 = 0
+        activation2 = 0
+        for word in counts:
+            if word in self.training_unique_words:
+                activation1 += self.weight_vector[0][word] * counts[word]
+                activation2 += self.weight_vector[1][word] * counts[word]
 
-        sent_vec = None
-        start_index = None
-        for index, word in enumerate(review):
-            if word in self.one_hot_vectors:
-                sent_vec = numpy.copy(self.one_hot_vectors[word])
-                start_index = index
-                break
-
-        if start_index is None:
-            return "True", "Pos"
-
-        for word in review[start_index:]:
-            if word in self.one_hot_vectors:
-                sent_vec += self.one_hot_vectors[word]
-
-        activation1 = numpy.matmul(sent_vec, self.weight_vector[0]) + self.bias[0]
-        activation2 = numpy.matmul(sent_vec, self.weight_vector[1]) + self.bias[1]
-
-        return "True" if activation1 > 0 else "Fake", "Pos" if activation2 > 0 else "Neg"
+        return "True" if (activation1 + self.bias[0]) > 0 else "Fake", "Pos" if (activation2 + self.bias[1]) > 0 else "Neg"
 
 if __name__ == "__main__":
     perceptron = PerceptronClassify(sys.argv[1], sys.argv[2], "percepoutput.txt")
--- perceplearn3.py
+++ perceplearn3.py
@@ -1,43 +1,18 @@
 from random import shuffle
-import numpy
-import time
 import sys
 import util3
+from collections import Counter
+import random
 
 class Data:
-
-    UNK = "unk"
-
-    def __init__(self, filename, mini_batch_size=1):
+    def __init__(self, filename):
         self.filename = filename
         self.reviews = {}
-        self.mini_batch_size = mini_batch_size
         self.unique_words = None
         self.feature_vectors = []
-        self.one_hot = {}
         self.read_corpus()
 
-    def one_hot_vectors(self):
-        for index, word in enumerate(self.unique_words):
-            zeros = numpy.zeros((1, len(self.unique_words)))
-            zeros.put(index, 1.0)
-            self.one_hot[word] = zeros
-
-    def form_sentence_vectors(self):
-        for identifier in self.reviews:
-            true_or_fake, pos_or_neg, review = self.reviews[identifier]
-            sent_vec = numpy.copy(self.one_hot[review[0]])
-            for word in review[1:]:
-                sent_vec += self.one_hot[word]
-
-            """ Converting expected outputs to binary values """
-            true_or_fake = 1 if true_or_fake == "True" else -1
-            pos_or_neg = 1 if pos_or_neg == "Pos" else -1
-
-            self.feature_vectors.append((true_or_fake, pos_or_neg, sent_vec))
-
     def read_corpus(self):
-        start = time.time()
         self.unique_words = set()
         with open(self.filename) as f:
             for line in f:
@@ -45,74 +20,78 @@ class Data:
                 identifier, true_or_fake, pos_or_neg, *review = line.strip().split()
                 review = list(map(str.lower, review))
                 self.reviews[identifier] = (true_or_fake, pos_or_neg, review)
+
+                """ Converting expected outputs to binary values """
+                true_or_fake = 1 if true_or_fake == "True" else -1
+                pos_or_neg = 1 if pos_or_neg == "Pos" else -1
+                self.feature_vectors.append((true_or_fake, pos_or_neg, review))
+
                 for word in review:
                     self.unique_words.add(word)
-        self.unique_words = list(self.unique_words)
-        self.unique_words.append(Data.UNK)
-
-        """ Get one hot encoding for word vectors """
-        self.one_hot_vectors()
-
-        """ Form sentence vectors --> feature_vectors """
-        self.form_sentence_vectors()
 
     """ Shuffle up the reviews to be considered in every epoch. """
     def shuffle(self):
         shuffle(self.feature_vectors)
-
-    """ In case we hav to implement mini-batching. """
-    def get_next_batch(self, index):
-        return self.reviews[index : min(index + self.mini_batch_size, len(self.reviews))], min(index + self.mini_batch_size, len(self.reviews))
 
 class Perceptron:
     def __init__(self, data, model_filename, epochs=5, is_average=False):
         self.data = data
         self.epochs = epochs
         """ Initialize weight vector """
-        self.weight_vector = [numpy.random.rand(len(data.unique_words), 1),
-                              numpy.random.rand(len(data.unique_words), 1)]
-        self.cached_weight_vector = [numpy.random.rand(len(data.unique_words), 1),
-                                     numpy.random.rand(len(data.unique_words), 1)]
+        self.weight_vector = [{}, {}]
+        self.cached_weight_vector = [{}, {}]
 
         """ Initialize bias """
-        self.bias = [numpy.random.random_sample(), numpy.random.random_sample()]
-        self.cached_bias = [numpy.random.random_sample(), numpy.random.random_sample()]
+        self.bias = [random.random(), random.random()]
+        self.cached_bias = [random.random(), random.random()]
 
         self.is_average_perceptron = is_average
         self.model_filename = model_filename
 
+    def initialise_weights(self):
+        for word in data.unique_words:
+            self.weight_vector[0][word] = random.random()
+            self.weight_vector[1][word] = random.random()
+            self.cached_weight_vector[0][word] = random.random()
+            self.cached_weight_vector[1][word] = random.random()
+
     def train(self):
         stopping_epoch = self.epochs
+        self.initialise_weights()
+        self.data.shuffle()
         for epoch in range(1, self.epochs):
             success = [0, 0]
-            self.data.shuffle()
             for tup in self.data.feature_vectors:
-                true_or_fake, pos_or_neg, vector = tup
+                true_or_fake, pos_or_neg, review = tup
                 """ We are training two classifiers here """
-                activation1 = numpy.matmul(vector, self.weight_vector[0]) + self.bias[0]
-                activation2 = numpy.matmul(vector, self.weight_vector[1]) + self.bias[1]
+                activation1 = 0
+                activation2 = 0
+                counts = Counter(review)
+                for word in counts:
+                    activation1 += self.weight_vector[0][word] * counts[word]
+                    activation2 += self.weight_vector[1][word] * counts[word]
 
                 """ Update weights for classifier 1 """
-                if true_or_fake * activation1 <= 0:
-                    self.weight_vector[0] += numpy.transpose(numpy.multiply(true_or_fake, vector))
+                if true_or_fake * (activation1 + self.bias[0]) <= 0:
+                    for word in counts:
+                        self.weight_vector[0][word] += true_or_fake * counts[word]
+                        if self.is_average_perceptron:
+                            self.cached_weight_vector[0][word] += true_or_fake * counts[word] * epoch
                     self.bias[0] += true_or_fake
-
                     if self.is_average_perceptron:
-                        self.cached_weight_vector[0] += numpy.transpose(numpy.multiply(numpy.multiply(true_or_fake, vector), epoch))
                         self.cached_bias[0] += true_or_fake * epoch
-
                 else:
                     success[0] += 1
 
                 """ Update weights for classifier 2 """
-                if pos_or_neg * activation2 <= 0:
-                    self.weight_vector[1] += numpy.transpose(numpy.multiply(pos_or_neg, vector))
+                if pos_or_neg * (activation2 + self.bias[1]) <= 0:
+                    for word in counts:
+                        self.weight_vector[1][word] += pos_or_neg * counts[word]
+                        if self.is_average_perceptron:
+                            self.cached_weight_vector[1][word] += pos_or_neg * counts[word] * epoch
                     self.bias[1] += pos_or_neg
-
                     if self.is_average_perceptron:
-                        self.cached_weight_vector[1] += numpy.transpose(numpy.multiply(numpy.multiply(pos_or_neg, vector), epoch))
                         self.cached_bias[1] += pos_or_neg * epoch
-
                 else:
                     success[1] += 1
 
@@ -123,8 +102,9 @@ class Perceptron:
             # print("Epoch:", epoch," Accuracy:", success[0] / len(self.data.feature_vectors), "% on True / Fake dataset and ",success[1] / len(self.data.feature_vectors), "% on Pos / Neg dataset")
 
         if self.is_average_perceptron:
-            self.weight_vector[0] = numpy.subtract(self.weight_vector[0], numpy.multiply(1 / (stopping_epoch + 1), self.cached_weight_vector[0]))
-            self.weight_vector[1] = numpy.subtract(self.weight_vector[1], numpy.multiply(1 / (stopping_epoch + 1), self.cached_weight_vector[1]))
+            for word in self.data.unique_words:
+                self.weight_vector[0][word] -= (1 / (stopping_epoch + 1)) * self.cached_weight_vector[0][word]
+                self.weight_vector[1][word] -= (1 / (stopping_epoch + 1)) * self.cached_weight_vector[1][word]
             self.bias[0] -= (1 / (stopping_epoch + 1)) * self.cached_bias[0]
             self.bias[1] -= (1 / (stopping_epoch + 1)) * self.cached_bias[1]
 
@@ -133,20 +113,12 @@ class Perceptron:
     def save(self):
         with open(self.model_filename, "w") as f:
             f.write(str(data.unique_words));f.write("\n")
-
-            w = []
-            for x in numpy.nditer(self.weight_vector[0]):
-                w.append(str(x))
-            f.write(str(w));f.write("\n")
-
-            w = []
-            for x in numpy.nditer(self.weight_vector[1]):
-                w.append(str(x))
-            f.write(str(w));f.write("\n")
+            f.write(str(self.weight_vector[0]));f.write("\n")
+            f.write(str(self.weight_vector[1]));f.write("\n")
             f.write(str(self.bias[0]));f.write("\n")
-            f.write(str(self.bias[1]));
+            f.write(str(self.bias[1]))
 
 if __name__ == "__main__":
     data = Data(sys.argv[1])
-    Perceptron(data, "vanillamodel.txt", epochs=20).train()
-    Perceptron(data, "averagedmodel.txt", epochs=20, is_average=True).train()
+    Perceptron(data, "vanillamodel.txt", epochs=100).train()
+    Perceptron(data, "averagedmodel.txt", epochs=100, is_average=True).train()
