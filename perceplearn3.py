25d63c3acd6c44729eefad14fb295b53e786a5f6
Sachin Malhotra <sachinma@usc.edu>
2018-04-22 14:52:33 -0700

    Modularized the code

--- perceplearn3.py
+++ perceplearn3.py
@@ -8,7 +8,8 @@ class Data:
     def __init__(self, filename):
         self.filename = filename
         self.unique_words = None
-        self.feature_vectors = []
+        self.feature_vectors_true_fake = []
+        self.feature_vectors_pos_neg = []
         self.read_corpus()
 
     def read_corpus(self):
@@ -21,13 +22,15 @@ class Data:
                 """ Converting expected outputs to binary values """
                 true_or_fake = 1 if true_or_fake == "True" else -1
                 pos_or_neg = 1 if pos_or_neg == "Pos" else -1
-                self.feature_vectors.append((true_or_fake, pos_or_neg, review))
+                self.feature_vectors_true_fake.append((true_or_fake, review))
+                self.feature_vectors_pos_neg.append((pos_or_neg, review))
 
                 for word in review:
                     self.unique_words.add(word)
     """ Shuffle up the reviews to be considered in every epoch. """
     def shuffle(self):
-        shuffle(self.feature_vectors)
+        shuffle(self.feature_vectors_pos_neg)
+        shuffle(self.feature_vectors_true_fake)
 
 class Perceptron:
     def __init__(self, data, model_filename, epochs=5, is_average=False):
@@ -51,51 +54,48 @@ class Perceptron:
             self.cached_weight_vector[0][word] = random.random()
             self.cached_weight_vector[1][word] = random.random()
 
+    def classify_and_update(self, review, weight, bias, cached_weight, cached_bias, output, epoch, is_average):
+        activation = 0
+        counts = Counter(review)
+        for word in counts:
+            activation += weight[word] * counts[word]
+
+        """ Update weights for classifier 1 """
+        if output * (activation + bias) <= 0:
+            for word in counts:
+                weight[word] += output * counts[word]
+                if is_average:
+                    cached_weight[word] += output * counts[word] * epoch
+                    bias += output
+            if is_average:
+                cached_bias += output * epoch
+            return False
+        else:
+            return True
+
     def train(self):
         stopping_epoch = self.epochs
         self.initialise_weights()
         self.data.shuffle()
         for epoch in range(1, self.epochs):
             success = [0, 0]
-            for tup in self.data.feature_vectors:
-                true_or_fake, pos_or_neg, review = tup
-                """ We are training two classifiers here """
-                activation1 = 0
-                activation2 = 0
-                counts = Counter(review)
-                for word in counts:
-                    activation1 += self.weight_vector[0][word] * counts[word]
-                    activation2 += self.weight_vector[1][word] * counts[word]
+            for tup1, tup2 in zip(self.data.feature_vectors_pos_neg, self.data.feature_vectors_true_fake):
+                true_or_fake, review_tf = tup2
+                pos_or_neg, review_pn = tup1
 
-                """ Update weights for classifier 1 """
-                if true_or_fake * (activation1 + self.bias[0]) <= 0:
-                    for word in counts:
-                        self.weight_vector[0][word] += true_or_fake * counts[word]
-                        if self.is_average_perceptron:
-                            self.cached_weight_vector[0][word] += true_or_fake * counts[word] * epoch
-                    self.bias[0] += true_or_fake
-                    if self.is_average_perceptron:
-                        self.cached_bias[0] += true_or_fake * epoch
-                else:
-                    success[0] += 1
+                """ review, weight, bias, cached_weight, cached_bias, output, epoch, is_average """
+                success[0] += True == self.classify_and_update(review_tf, self.weight_vector[0], self.bias[0], self.cached_weight_vector[0]
+                                         , self.cached_bias[0], true_or_fake, epoch, self.is_average_perceptron)
+                success[1] += True == self.classify_and_update(review_pn, self.weight_vector[1], self.bias[1], self.cached_weight_vector[1]
+                                         , self.cached_bias[1], pos_or_neg, epoch, self.is_average_perceptron)
 
-                """ Update weights for classifier 2 """
-                if pos_or_neg * (activation2 + self.bias[1]) <= 0:
-                    for word in counts:
-                        self.weight_vector[1][word] += pos_or_neg * counts[word]
-                        if self.is_average_perceptron:
-                            self.cached_weight_vector[1][word] += pos_or_neg * counts[word] * epoch
-                    self.bias[1] += pos_or_neg
-                    if self.is_average_perceptron:
-                        self.cached_bias[1] += pos_or_neg * epoch
-                else:
-                    success[1] += 1
+
 
             """ Classified completely """
-            if success[0] == len(self.data.feature_vectors) and success[0] == success[1]:
+            if success[0] == len(self.data.feature_vectors_true_fake) and success[0] == success[1]:
                 stopping_epoch = epoch
                 break
-            # print("Epoch:", epoch," Accuracy:", success[0] / len(self.data.feature_vectors), "% on True / Fake dataset and ",success[1] / len(self.data.feature_vectors), "% on Pos / Neg dataset")
+            # print("Epoch:", epoch," Accuracy:", success[0] / len(self.data.feature_vectors_true_fake), "% on True / Fake dataset and ",success[1] / len(self.data.feature_vectors_true_fake), "% on Pos / Neg dataset")
 
         if self.is_average_perceptron:
             for word in self.data.unique_words:
